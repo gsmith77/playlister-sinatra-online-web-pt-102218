1fa1cf589cb1d5df872f2aec840b3c946c362a8f
grant smith <grantsmith1256@gmail.com>
2019-03-03 20:26:36 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190303200230) do
+ActiveRecord::Schema.define(version: 20190303195000) do
 
   create_table "artists", force: :cascade do |t|
-    t.string "name"
-  end
-
-  create_table "genres", force: :cascade do |t|
-    t.string "name"
-  end
-
-  create_table "songs", force: :cascade do |t|
     t.string "name"
   end
 
