8d19d0870557bd86cd1728fe67b5abd81a9821eb
grant smith <grantsmith1256@gmail.com>
2019-03-03 21:01:55 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -20,13 +20,11 @@ ActiveRecord::Schema.define(version: 20190303205216) do
   create_table "genres", force: :cascade do |t|
     t.string  "name"
     t.integer "song_id"
-    t.integer "artist_id"
   end
 
   create_table "songs", force: :cascade do |t|
     t.string  "name"
     t.integer "artist_id"
-    t.integer "genre_id"
   end
 
 end
